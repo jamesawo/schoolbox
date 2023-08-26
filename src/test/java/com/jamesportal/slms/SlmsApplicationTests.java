269239d6c3e7eeef2e6456319fdc338de722b9b6
James Aworo <james.aworo@outlook.com>
2023-08-26 04:06:39 +0100

    wip(test): add base test directory

---
+++ src/test/java/com/jamesportal/slms/SlmsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jamesportal.slms;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SlmsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
